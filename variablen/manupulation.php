a32752e91fc682e8d0821c6ba2be8d77fa70caba
tBrause <brause.torsten@gmail.com>
2022-05-25 02:46:01 +0200

    y

--- variablen/manupulation.php
+++ variablen/manupulation.php
@@ -1,24 +1,34 @@
 <?php
-# Typen von PHP Variablen
+# HANDLING von PHP Variablen
 ##############################
-# string, integer, double, boolean, array, object
 
-// string in integer
+# string in integer
+##############################
 $var = '5';
 echo intval($var) . '<br>';
 
-// integer
-$var = 5;
-echo gettype($var) . '<br>';
+# mit verschiedenen Typen rechnen
+##############################
+$var = 5545.2756;
+$var2 = 2;
+$var3 = '2 ddd';
 
-// double
-$var = 5425.2756;
-$var2 = 2.3;
-echo gettype($var) . '<br>';
-echo ($var * $var2) . '<br>';
+# sind Zeichen enthalten
+if (!is_numeric($var3)) {
+    echo 'Zeichen enthalten<br>';
+}
 
-# round
-echo round($var, 1) . '<br>';
+echo '$var : ' . gettype($var) . ' [' . $var . ']<br>';
+echo '$var2 : ' . gettype($var2) . ' [' . $var2 . ']<br>';
+echo '$var3 : ' . gettype($var3) . ' [' . $var3 . ']<br>';
+echo '$var3 : ' . gettype((float)$var3) . ' [' . (float)$var3 . ']<br>';
 
-# deutsches Zahlenformat
-echo number_format($var, 2, ',', '.') . '<br>';
+# Werte addieren
+$sum = (float)$var + (float)$var2 + (float)$var3;
+echo '$sum : ' . $sum . '<br>';
+
+# Wert round
+echo 'round : ' . round($sum, 2) . '<br>';
+
+# Wert deutsches Zahlenformat
+echo 'number_format : ' . number_format($sum, 2, ',', '.') . '<br>';
